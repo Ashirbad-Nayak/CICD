33c42254390f2cbadd729821caf8077d1c281b35
Ashirbad <ashirbadnayakofficial97@gmail.com>
2025-04-22 15:19:22 +0530

    CICD With AWS ECS

---
+++ index.js
@@ -0,0 +1,24 @@
+
+
+const express = require('express');
+const app = express();
+const port = 3000;
+
+
+// Define a route
+app.get('/', (req, res) => {
+res.send('Congratulations! First CICD with AWS ECS');
+});
+
+
+app.get('/health', (req, res) => {
+   res.status(200).send('ECS  is healthy');
+});
+
+
+
+
+// Start the server
+app.listen(port, () => {
+console.log(`Server is listening at http://localhost:${port}`);
+});
